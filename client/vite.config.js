3f531ab2ad9f1f2b9a1ab0dc045d07aafb98da31
Karthik N P <79256465+karthik-n-p@users.noreply.github.com>
2025-04-22 00:46:42 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import fs from 'fs';
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react() ,
     tailwindcss()
   ],
-  server: {
-    https: {
-      key: fs.readFileSync('./certs/key.pem'),
-      cert: fs.readFileSync('./certs/cert.pem')
-    },
     port: 3000,
     host: true
   }
